ea90912d7547f7a4a73702fb42af987f03ed3320
skrall3119 <ajanci429@students.mchenry.edu>
2018-02-16 13:38:07 -0600

    Add files via upload

--- Practice 5.2.py
+++ Practice 5.2.py
@@ -1,25 +1,27 @@
-def main(): #The main function that gets the input from the user and prints out the costs per month and costs per year
+def main():  # The main function that gets the input from the user and prints out the costs per month and costs per year
     loan = int(input("Enter your monthly loan payment: "))
     insurance = int(input("Enter your monthly insurance payment: "))
     gas = int(input("Enter the amount you spend on gas monthly: "))
     tires = int(input("Enter the amount you spend on tires monthly: "))
     maintenance = int(input("Enter the cost to maintain your vehicle: "))
 
-    monthlyCosts = addCosts(loan, insurance, gas, tires, maintenance)
-    yearlyCosts = calculateYearlyCost(monthlyCosts)
-    printCosts(yearlyCosts, monthlyCosts)
+    monthly_costs = add_costs(loan, insurance, gas, tires, maintenance)
+    yearly_costs = calculate_yearly_cost(monthly_costs)
+    print_costs(yearly_costs, monthly_costs)
 
 
-def addCosts(a, b, c, d, e): #adds the amounts given by the user
-    monthlytotal = a + b + c + d + e
-    return int(monthlytotal)
+def add_costs(a, b, c, d, e):  # adds the amounts given by the user
+    monthly_total = a + b + c + d + e
+    return int(monthly_total)
 
-def calculateYearlyCost(monthlytotal): #calculates the yearly cost with the value returned by addCosts
-    yearlyCost = monthlytotal * 12
-    return yearlyCost
 
-def printCosts(yearlyCost, monthlyCost): #prints the costs calculated in addCosts and calculateYearlyCost
-    print("The monthly total for your vehicle is:", format(monthlyCost, ',.2f'))
-    print("The yearly total for your vehicle is:", format(yearlyCost, ',.2f'))
+def calculate_yearly_cost(monthly_total):  # calculates the yearly cost with the value returned by add_costs
+    yearly_cost = monthly_total * 12
+    return yearly_cost
+
+
+def print_costs(yearly_cost, monthly_cost):  # prints the costs calculated in add_costs and calculate_yearly_cost
+    print("The monthly total for your vehicle is:", format(monthly_cost, ',.2f'))
+    print("The yearly total for your vehicle is:", format(yearly_cost, ',.2f'))
 
 main()
--- Practice 5.4.py
+++ Practice 5.4.py
@@ -1,30 +1,29 @@
-def fatCalories(fat): # Calculates calories from fat
-    return fat * 4
+def fat_calories(fat):  # Calculates calories from fat
+    return fat * 9
 
 
-def carbCalories(carb): # Calculates calories from carbohydrates
+def carb_calories(carb):  # Calculates calories from carbohydrates
     return carb * 4
 
 
-def proteinCalories(protein): # calculates calories from protein
+def protein_calories(protein):  # calculates calories from protein
     return protein * 4
 
 
-def printTotals():# prints out calculations and total
-    print("\nthe number of calories from fat is:", fatCalories(fatGrams))
-    print("The number of calories from carbohydrates is:", carbCalories(carbGrams))
-    print("The number of calories from protein is:", proteinCalories(proteinGrams))
+def print_totals():  # prints out calculations and total
+    print("\nthe number of calories from fat is:", fat_calories(fat_grams))
+    print("The number of calories from carbohydrates is:", carb_calories(carb_grams))
+    print("The number of calories from protein is:", protein_calories(protein_grams))
     print("The total number of calories is:", calories)
 
-calories = 0 # initialize total calorie count
 
- # takes input from user
-fatGrams = int(input("Enter the amount of grams of fat you ate: "))
-carbGrams = int(input("Enter the amount of grams of carbohydrates you ate: "))
-proteinGrams = int(input("Enter the amount of grams of protein you ate: "))
+#  takes input from user
+fat_grams = int(input("Enter the amount of grams of fat you ate: "))
+carb_grams = int(input("Enter the amount of grams of carbohydrates you ate: "))
+protein_grams = int(input("Enter the amount of grams of protein you ate: "))
 
 # Calculates total calories
-calories = fatCalories(fatGrams) + carbCalories(carbGrams) + proteinCalories(proteinGrams)
-printTotals()
+calories = fat_calories(fat_grams) + carb_calories(carb_grams) + protein_calories(protein_grams)
+print_totals()
 
 
