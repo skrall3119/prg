fb324b3ffc4f7b714f72544b577361ed58b25d5a
skrall3119 <ajanci429@students.mchenry.edu>
2018-04-17 20:35:14 -0500

    Add files via upload

---
+++ Practice 12.1.py
@@ -0,0 +1,14 @@
+# Prints all numbers starting at one to n
+def count(n):
+    num = 1
+    while num <= n:
+        print(num)
+        num += 1
+
+
+# Main program
+def main():
+    count(5)
+
+
+main()
