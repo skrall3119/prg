c4e2a0d0cef0a0e2fa5039d39b77af90b6f237c2
skrall3119 <ajanci429@students.mchenry.edu>
2018-04-17 19:33:42 -0500

    Update Practice 12.2.py

--- Practice 12.2.py
+++ Practice 12.2.py
@@ -1,12 +1,12 @@
-def find_sum(list):
-    if len(list) == 1:
-        return list[0]
+def find_sum(nums):
+    if len(nums) == 1:
+        return nums[0]
     else:
-        return list[0] + find_sum(list[1:])
+        return nums[0] + find_sum(nums[1:])
 
 
 def main():
-    list = [2, 7, 43, 15, 76, 22]
-    print(find_sum(list))
+    nums = [2, 7, 43, 15, 76, 22]
+    print(find_sum(nums))
 
 main()
